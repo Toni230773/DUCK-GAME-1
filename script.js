9d072af564b3e9e7310aba1a7cb607f51dc0c4eb
Toni230773 <tonigalmestruyols@gmail.com>
2025-06-08 01:06:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 
+// script.js
+const synth = window.speechSynthesis;
+let mouthInterval;
+
+function speak(text) {
+  if (!('speechSynthesis' in window)) return alert('Your browser does not support speech synthesis.');
+  const utter = new SpeechSynthesisUtterance(text);
+  utter.lang = 'en-US';
+  utter.onstart = animateMouth;
+  utter.onend = stopMouth;
+  synth.speak(utter);
+}
+
+function animateMouth() {
+  const mouth = document.querySelector('.mouth');
+  mouthInterval = setInterval(() => {
+    mouth.classList.toggle('open');
+  }, 200); // toggle every 200ms
+}
+
+function stopMouth() {
+  clearInterval(mouthInterval);
+  document.querySelector('.mouth').classList.remove('open');
+}
